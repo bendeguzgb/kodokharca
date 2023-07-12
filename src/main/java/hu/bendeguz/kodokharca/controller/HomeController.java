10ea72cb105476638d4648914e4bef8c98ec3c77
Gerencsér Bendegúz <bendeguz.gb@gmail.com>
2023-07-12 02:09:59 +0200

    feat(ui): Add script to handle combination filtering questions

--- src/main/java/hu/bendeguz/kodokharca/controller/HomeController.java
+++ src/main/java/hu/bendeguz/kodokharca/controller/HomeController.java
@@ -3,6 +3,7 @@ package hu.bendeguz.kodokharca.controller;
 import hu.bendeguz.kodokharca.model.GameNumber;
 import hu.bendeguz.kodokharca.service.CombinationGenerator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class HomeController {
     private static final String HOME_SCREEN = "home";
     private static final int ELEMENTS_IN_ARRAY_5_NUMBERS = 5;
@@ -21,7 +23,12 @@ public class HomeController {
 
 
     @GetMapping(value = {"/", "/home"})
-    public String showHome(@RequestParam(required = false, defaultValue = "5") Integer elementsInArray, Model model) {
+    public String showHome(@RequestParam(required = false, defaultValue = "5") Integer elementsInArray,
+        @RequestParam(required = false) List<String> filter, Model model) {
+
+        log.debug("filter = '{}'", filter);
+        log.debug("elementsInArray = '{}' ", elementsInArray);
+
         model.addAttribute("combinations", CombinationGenerator.generateAllCombinations(elementsInArray));
         model.addAttribute("elementsInArray", elementsInArray);
         model.addAttribute("headerIterable", new boolean[elementsInArray]);
--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,15 +1,22 @@
 const PARAMETER_SEPARATOR = "&";
 const ELEMENTS_IN_ARRAY = "elementsInArray=";
+const FILTER_STRING = "filter=";
 const SEARCH = window.location.search;
 
-// ?elementsInArray=5&
-if (SEARCH.match(`^\\?${ELEMENTS_IN_ARRAY}\\d`) === null) {
+// ?elementsInArray=5&filter=
+if (SEARCH.match(`^\\?${ELEMENTS_IN_ARRAY}\\d${PARAMETER_SEPARATOR}${FILTER_STRING}`) === null) {
     console.log(`Search: '${SEARCH}'`)
     console.log("Fixing parameters...")
     let searchCopy = SEARCH;
 
     if (!searchCopy.includes(ELEMENTS_IN_ARRAY)) {
         searchCopy = `?${ELEMENTS_IN_ARRAY}5${PARAMETER_SEPARATOR}${searchCopy.substring(1)}`
+    }
+
+    if (!searchCopy.includes(FILTER_STRING)) {
+        let param = (searchCopy.endsWith(PARAMETER_SEPARATOR)) ? "" : PARAMETER_SEPARATOR;
+        param += FILTER_STRING;
+        searchCopy += param;
     }
 
     if (window.location.search !== searchCopy) {
@@ -23,8 +30,8 @@ window.addEventListener("DOMContentLoaded", function() {
     countOfElements = countOfElements.substring(countOfElements.length - 1);
     document.getElementById(`elements${countOfElements}`).checked = true;
 
-
-    const elementsInArrayForm = document.getElementById("elementsInArrayContainer");
+    // override 'onsubmit' of the selector of number of player's numbers
+    const elementsInArrayForm = document.getElementById("elementsInArrayForm");
     elementsInArrayForm.onsubmit = function () {
         for (let input of elementsInArrayForm.getElementsByTagName("input")) {
             if (input.type !== "radio") {
@@ -40,5 +47,54 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         }
         return false;
+    }
+
+    // override 'onsubmit' functions of questions
+    for (let form of document.getElementsByClassName('question')) {
+        form.onsubmit = function() {
+            const id = this.id;
+
+            let filterIndex;
+            let currentFilters;
+            let parameters = SEARCH.split(PARAMETER_SEPARATOR);
+
+            for (let i= 0; i < parameters.length; i++) {
+                if (parameters[i].includes(FILTER_STRING)) {
+                    currentFilters = parameters[i];
+                    filterIndex = i;
+                    break;
+                }
+            }
+
+            if (currentFilters.includes(id)) {
+                alert(`Ez a szűrés már megtörtént: '${id}'\nKeresések: '${currentFilters}'`);
+                return false;
+            }
+
+            let filter = (currentFilters.endsWith("=")) ? id : `,${id}`;
+            let isRadioChecked = false;
+
+            for (let input of this.getElementsByTagName("input")) {
+                if (input.type === "submit" || isRadioChecked) {
+                    continue;
+                }
+
+                if (input.type === "radio") {
+                    let isOn = input.checked;
+                    let isFound = ["páros", "fekete", "igen"].includes(input.labels[0].innerText.toLowerCase());
+
+                    filter += `-${isFound && isOn || !isFound && !isOn}`;
+                    isRadioChecked = true;
+                } else {
+                    filter += `-${input.value}`;
+
+                }
+            }
+
+            parameters[filterIndex] = currentFilters + filter;
+            window.location.search = parameters.join(PARAMETER_SEPARATOR);
+
+            return false;
+        };
     }
 });
